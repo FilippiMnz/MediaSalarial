45f8e66fff549c48fee187888670cbec83698bc3
FilippiMnz <imperilkill12@gmail.com>
2022-12-13 22:38:07 -0300

    Kenpachi

---
+++ Salario/Program.cs
@@ -0,0 +1,24 @@
+﻿using System; 
+
+namespace Salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o nome do Funcionario(a):");
+            string nome1 = Console.ReadLine();
+            Console.Write("Insira o Salario");
+            int salario = int.Parse(Console.ReadLine());
+
+            Console.Write("Insira o nome do Funcionario(a):");
+            string nome2 = Console.ReadLine();
+            Console.Write("Insira o Salario");
+            int salario2 = int.Parse(Console.ReadLine());
+
+            int mediaSalarial = (salario + salario2)/2;
+                
+            Console.WriteLine("A media Salarial é de " + mediaSalarial);
+        }
+    }
+}
